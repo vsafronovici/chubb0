bdb9799e85d246d5c2107fe7fcc9432c69500bc0
igor.chistruga <ichistruga@endava.com>
2016-10-11 12:00:36 +0300

    Added some files

---
+++ rest-adapter/src/main/java/com/chubb/rest/adapter/Application.java
@@ -0,0 +1,7 @@
+package com.chubb.rest.adapter;
+
+/**
+ * Created by ichistruga on 10/11/2016.
+ */
+public class Application {
+}
---
+++ rest-adapter/src/test/java/MainTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ichistruga on 10/11/2016.
+ */
+public class MainTest {
+}
