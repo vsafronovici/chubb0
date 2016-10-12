efb3b04bb372be4f0c5bb2d56ea6acf9cb4ecab8
vsafronovici <Vitalie.Safronovici@endava.com>
2016-10-12 11:30:35 +0300

    added critical exception

---
+++ rest-adapter/src/main/java/com/chubb/rest/adapter/exception/CriticalException.java
@@ -0,0 +1,11 @@
+package com.chubb.rest.adapter.exception;
+
+/**
+ * Created by vsafronovici on 10/12/2016.
+ */
+public class CriticalException extends RuntimeException {
+
+    public CriticalException(String message, Throwable e) {
+        super(message, e);
+    }
+}
